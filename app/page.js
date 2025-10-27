fbb12b14ee88e767aa941afee11814659b0b6912
Hamidul islam <hi6139642@gmail.com>
2025-10-27 16:37:57 +0530

    10/27 commit

--- app/page.js
+++ app/page.js
@@ -3,8 +3,8 @@ import Navbar from "./components/Navbar";
 export default function Home() {
   return (
     <>
-    <Navbar/>
-       <section>
+      <Navbar />
+      <section>
         <div className="hero-section container mx-auto ">
           <div className="main flex items-center justify-center flex-col h-90  gap-5">
             <p className="font-bold text-5xl">
@@ -20,9 +20,9 @@ export default function Home() {
         </div>
       </section>
       <section className="section2 flex items-center justify-center">
-       
-          <button type="button" className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Cyan to Blue</button>
-         
+
+        <button type="button" className="text-white bg-gradient-to-r w-sm from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Read the quotes</button>
+
       </section>
     </>
   );
