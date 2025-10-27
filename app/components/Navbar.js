4894b2aff4f1f5645ce07926b358be22b7bd3b81
Hamidul islam <hi6139642@gmail.com>
2025-10-27 16:35:10 +0530

    10/27/25 commit

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,56 +1,58 @@
-"use client"
-import React from 'react'
-import { useState } from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
-import Dropdown from './Dropdown'
+"use client";
+import React from "react";
+import { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import Dropdown from "./Dropdown";
 const Navbar = () => {
-  const [isAsideOpen, setIsAsideOpen] = useState(false);
+  const [isAsideOpen, setIsAsideOpen] = useState(true);
   const openAside = () => {
     setIsAsideOpen(!isAsideOpen);
   };
   return (
-
     <>
-
-
-
       {/* Navbar */}
       <nav>
         <div className="navbar flex items-center gap-4 p-3 bg-sky-100">
           <div className="hamburger cursor-pointer" onClick={openAside}>
-            <Image
-              src="/menu.svg"
-              alt='menu-icon'
-              width="25"
-              height="25"
-            />
+            <Image src="/menu.svg" alt="menu-icon" width="25" height="25" />
           </div>
           <div className="logo">
-            <h1 className='font-bold text-2xl flex items-center '>
-              <span className='text-green-700'>&lt;</span>QUOTES/<span className='text-green-700'>&gt;</span>
+            <h1 className="font-bold text-2xl flex items-center ">
+              <span className="text-green-700">&lt;</span>QUOTES/
+              <span className="text-green-700">&gt;</span>
             </h1>
           </div>
         </div>
       </nav>
+
       {/* navigationn aside area */}
       <aside
         className={`fixed  top-14 left-0 h-full w-64 bg-blue-50 shadow-lg z-50 transform transition-transform duration-300 ${isAsideOpen ? "translate-x-0" : "-translate-x-full"
           }`}
       >
-        <ul className="p-4 flex flex-col gap-2 ">
-          <li className='text-center flex rounded-4xl hover:bg-blue-700/30 hover:backdrop-blur-md transition-all duration-300 h-10 justify-center items-center'><Link href="/" ></Link>Home</li>
-          <li className='text-center flex rounded-4xl hover:bg-blue-700/30 hover:backdrop-blur-md transition-all duration-300 h-10 justify-center items-center'><Dropdown/> </li>
-                    <li className='text-center flex rounded-4xl hover:bg-blue-700/30 hover:backdrop-blur-md transition-all duration-300 h-10 justify-center items-center'><Link href="/" ></Link>Contact</li>
-                    <li className='text-center flex rounded-4xl hover:bg-blue-700/30 hover:backdrop-blur-md transition-all duration-300 h-10 justify-center items-center'><Link href="/" ></Link>Write your Quotes</li>
+        <ul className="p-4 flex flex-col gap-2 text-lg">
+          <li className="text-center flex rounded-4xl hover:bg-blue-700/30 hover:backdrop-blur-md transition-all duration-300 h-10 justify-center items-center">
+            <Link href="/"></Link>Home
+          </li>
+            <li className="text-center flex rounded-4xl hover:bg-blue-700/30 hover:backdrop-blur-md transition-all duration-300 h-10 justify-center items-center">
+              <Link href="/"></Link>Read Quotes
+            </li>
+          <li className="text-center flex rounded-4xl hover:bg-blue-700/30 hover:backdrop-blur-md transition-all duration-300 h-10 justify-center items-center">
+            <Dropdown />{" "}
+          </li>
+          <li className="text-center flex rounded-4xl hover:bg-blue-700/30 hover:backdrop-blur-md transition-all duration-300 h-10 justify-center items-center">
+            <Link href="/"></Link>Contact
+          </li>
+          <li className="text-center flex rounded-4xl hover:bg-blue-700/30 hover:backdrop-blur-md transition-all duration-300 h-10 justify-center items-center">
+            <Link href="/"></Link>Write your Quotes
+          </li>
         </ul>
       </aside>
-          <section>
 
-            
-          </section>
+   
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
