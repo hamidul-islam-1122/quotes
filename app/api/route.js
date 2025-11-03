2cfa31060b0f601834239132987f15d93c350a2f
Hamidul islam <hi6139642@gmail.com>
2025-11-03 23:17:52 +0530

    commit

--- app/api/route.js
+++ app/api/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function POST() {
   console.log("✅ API route hit!");
-  const res = await fetch(" https://catfact.ninja/fact");
+  const res = await fetch("https://zenquotes.io/api/random");
   const data = await res.json();
   const quote = Array.isArray(data) ? data[0] : data;
   return NextResponse.json(quote);
--- app/readquotes/page.js
+++ app/readquotes/page.js
@@ -3,29 +3,33 @@ import React, { useState } from "react";
 
 export default function Page() {
   const [quotes, setQuotes] = useState([]);
-
+  const [loading, setLoading] = useState(false)
   const fetchQuotes = async () => {
-    const res = await fetch("/api", { method: "POST" });
-    const data = await res.json();
-    setQuotes((prev) => [...prev, data]);
+    setLoading(true);
+    try {
+      const res = await fetch("/api", { method: "POST" });
+      const data = await res.json();
+      setQuotes((prev) => [...prev, data]);
+    } catch (err) {
+      console.error("Error fetching quote:", err);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
-    <main className="p-6">
+    <main className="p-6 flex flex-col items-center">
       <h1 className="font-bold text-3xl mb-4">Read Quotes</h1>
-      <button
-        onClick={fetchQuotes}
-        className="bg-blue-500 text-white px-4 py-2 rounded"
-      >
-        Get Quote
-      </button>
-
+    <button
+    onClick={fetchQuotes}
+        disabled={loading}
+    type="button" className="text-white cursor-pointer bg-gradient-to-r  from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">{loading ? "loading ..." : "Fetch Quote"}</button>
       <p className="text-red-500"></p>
 
       {quotes.map((q, i) => (
-        <div key={i} className="bg-gray-100 p-4 mt-4 rounded">
-          <p className="italic mb-2">"{q.fact}"</p>
-          <p>— {q.length}</p>
+        <div key={i} className="bg-gray-100 px-10 mt-4 rounded ">
+          <p className="italic mb-2">"{q.q}"</p>
+          <p>— {q.a}</p>
         </div>
       ))}
     </main>
